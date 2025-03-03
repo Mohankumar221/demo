6ca7f9ad8116298d7b2458000a1194e19afdf56f
Mohan Kumar P1 <mohankumar_p1@epam.com>
2025-03-03 20:30:58 +0530

    Syndicate initialization with java Lets Try

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,10 +19,12 @@ import java.util.Map;
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
+
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			System.out.println("Hello from lambda");
+			resultMap.put("statusCode", 200);
+			resultMap.put("message", "Hello from Lambda");
+
 		return resultMap;
 	}
 }
